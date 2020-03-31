07e46b4f3527c91eb07770d0492f8eca9aceea76
LiorRozenblit <liorroz26112000@gmail.com>
2020-03-31 12:18:19 +0300

    initial version

--- stam_stam.py
+++ stam_stam.py
@@ -1,36 +1,25 @@
 import sys
-FileNameRead = sys.argv[1]
-FileNameWrite = sys.argv[2]
-Count = 0
-Write = []
-CurrentLine = ''
-def change_list(Count):
-    number = open(FileNameRead, 'r')
-    for line in number:
-        Count += 1
-    number.close()
-    number1 = open(FileNameRead, 'r')
-    for i in range(Count):
-       append_list = number1.readline()
-       Write.append(append_list)
-    number1.close()
-
-
-def change_file(Count,):
-    append = open(FileNameWrite, 'w')
-    for i in range(1, Count):
-        CurrentLine = Write[i-1]
-        append.write('Lior ' + CurrentLine.strip('\n') + ' Rozenblit\n')
-    append.close()
-
-
-def main():
-    change_list(Count)
-    change_file(Count)
-    readFile = open(FileNameWrite, 'r')
-    print_new_file = readFile.read()
-    print(print_new_file)
-
-
-if __name__ == '__main__':
-    main()
+file_name = sys.argv[1]
+file_name2 = sys.argv[2]
+number_of_lines = None
+count = 0
+write = []
+current_line = ''
+number = open(file_name, 'r')
+while number_of_lines != '':
+    number_of_lines = number.readline()
+    count += 1
+number.close()
+number1 = open(file_name, 'r')
+for i in range(count):
+    append_list = number1.readline()
+    write.append(append_list)
+number1.close()
+append = open(file_name2, 'w')
+for i in range(1, count):
+    current_line = write[i-1]
+    append.write('Lior ' + current_line.strip('\n') + ' Rozenblit\n')
+append.close()
+readFile = open(file_name2, 'r')
+PrintFile = readFile.read()
+print(PrintFile)
