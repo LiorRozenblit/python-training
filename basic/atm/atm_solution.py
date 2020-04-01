8dab54b8f5b257e55f29f6b6e9d6fcec678af399
LiorRozenblit <liorroz26112000@gmail.com>
2020-04-01 14:36:58 +0300

    second version

--- basic/atm/atm_solution.py
+++ basic/atm/atm_solution.py
@@ -13,10 +13,51 @@ customer_id = input("please enter your customer id:")
 for i in range(Count):
     if customer_id == my_list[i]:
         customer["balance"] = my_list[i + 1]
-        customer["password"] = my_list[i+2]
-ATM_password = input("please enter your ATM password id:")
-if ATM_password == customer["password"]:
-    print("true")
-else:
-    print("false")
-print(customer)
+        customer["password"] = my_list[i + 2]
+
+
+def check_balance():
+    print('you have {} money in your balance'.format(customer["balance"]))
+    return customer["balance"]
+
+
+def cash_withdrawl():
+    cash_to_withdrawl = input("please enter amount of money you want to withdrawl:")
+    print('you withdrawl {} money'.format(cash_to_withdrawl))
+    customer["balance"] -= cash_to_withdrawl
+    return customer["balance"]
+
+
+def cash_deposit():
+    cash_to_deposit = input("please enter amount of money you want to deposit:")
+    print('you deposit {} money'.format(cash_to_deposit))
+    customer["balance"] += cash_to_deposit
+    return customer["balance"]
+
+
+def change_password():
+    new_password = input("please enter new password:")
+    print('your new password is {}'.format(new_password))
+    customer["password"] = new_password
+    return customer["password"]
+
+
+def main():
+    atm_password = input("please enter your ATM password:")
+    while atm_password != customer["password"]:
+        atm_password = input("please enter your ATM password:")
+    while customer_id != -1:
+        customer_action = input(
+            "please choose between the actions: change password, cash deposit, cash withdrawl, check balance:")
+        if customer_action == "change password":
+            change_password()
+        if customer_action == "cash deposit":
+            cash_deposit()
+        if customer_action == "cash withdrawl":
+            cash_withdrawl()
+        if customer_action == "check balance":
+            check_balance()
+
+
+if __name__ == '__main__':
+    main()
