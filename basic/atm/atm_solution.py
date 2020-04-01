9d2a34e581bcc968017fadff3942757054c36499
LiorRozenblit <liorroz26112000@gmail.com>
2020-04-01 15:29:49 +0300

    third version

--- basic/atm/atm_solution.py
+++ basic/atm/atm_solution.py
@@ -1,4 +1,5 @@
 import sys
+
 customer = {"password": 0, "balance": 0}
 input_file = sys.argv[1]
 my_list = []
@@ -21,18 +22,30 @@ def check_balance():
     return customer["balance"]
 
 
-def cash_withdrawl():
-    cash_to_withdrawl = input("please enter amount of money you want to withdrawl:")
-    print('you withdrawl {} money'.format(cash_to_withdrawl))
-    customer["balance"] -= cash_to_withdrawl
-    return customer["balance"]
+def cash_withdrawal():
+    cash_to_withdrawal = input("please enter amount of money you want to withdrawal:")
+    try:
+        a = int(cash_to_withdrawal)
+        b = int(customer["balance"])
+        c = b - a
+        customer["balance"] = c
+        print('you withdrawal {} money'.format(cash_to_withdrawal))
+        return customer["balance"]
+    except:
+        return customer["balance"]
 
 
 def cash_deposit():
     cash_to_deposit = input("please enter amount of money you want to deposit:")
-    print('you deposit {} money'.format(cash_to_deposit))
-    customer["balance"] += cash_to_deposit
-    return customer["balance"]
+    try:
+        a = int(cash_to_deposit)
+        b = int(customer["balance"])
+        c = b + a
+        customer["balance"] = c
+        print('you deposit {} money'.format(cash_to_deposit))
+        return customer["balance"]
+    except:
+        return customer["balance"]
 
 
 def change_password():
@@ -42,22 +55,23 @@ def change_password():
     return customer["password"]
 
 
-def main():
+def main(customer_id):
     atm_password = input("please enter your ATM password:")
     while atm_password != customer["password"]:
         atm_password = input("please enter your ATM password:")
-    while customer_id != -1:
+    while customer_id != "-1":
         customer_action = input(
-            "please choose between the actions: change password, cash deposit, cash withdrawl, check balance:")
+            "please choose between the actions: change password, cash deposit, cash withdrawal, check balance:")
         if customer_action == "change password":
             change_password()
         if customer_action == "cash deposit":
             cash_deposit()
-        if customer_action == "cash withdrawl":
-            cash_withdrawl()
+        if customer_action == "cash withdrawal":
+            cash_withdrawal()
         if customer_action == "check balance":
             check_balance()
+        customer_id = input("please enter -1 if you want to close ATM:")
 
 
 if __name__ == '__main__':
-    main()
+    main(customer_id)
