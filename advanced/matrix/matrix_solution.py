0a61ddcfeb0b45eb056eef0593f76a2e4d047982
LiorRozenblit <liorroz26112000@gmail.com>
2020-04-15 21:22:48 +0300

    last version

--- advanced/matrix/matrix_solution.py
+++ advanced/matrix/matrix_solution.py
@@ -37,43 +37,42 @@ class Matrix:
     def __add__(self, other):
         add = []
         add2 = []
-        y = self.tuples()
-        x = other.tuples()
-        for i in range(len(y[1])):
-            for j in range(len(y[1])):
-                add.append(y[i][j] + x[i][j])
+        tupleOfSelf = self.tuples()
+        tupleOfOther = other.tuples()
+        for i in range(len(tupleOfSelf[1])):
+            for j in range(len(tupleOfSelf[1])):
+                add.append(tupleOfSelf[i][j] + tupleOfOther[i][j])
             add2.append(tuple(add))
             add = []
         return 'Matrix({})'.format(tuple(add2))
 
     def __sub__(self, other):
-        add = []
-        add2 = []
-        y = self.tuples()
-        x = other.tuples()
-        for i in range(len(y[1])):
-            for j in range(len(y[1])):
-                add.append(y[i][j] - x[i][j])
-            add2.append(tuple(add))
-            add = []
-        return 'Matrix({})'.format(tuple(add2))
+        sub = []
+        sub2 = []
+        tupleOfSelf = self.tuples()
+        tupleOfOther = other.tuples()
+        for i in range(len(tupleOfSelf[1])):
+            for j in range(len(tupleOfSelf[1])):
+                sub.append(tupleOfSelf[i][j] - tupleOfOther[i][j])
+            sub2.append(tuple(sub))
+            sub = []
+        return 'Matrix({})'.format(tuple(sub2))
 
     def __truediv__(self, other):
-        add = []
-        add2 = []
-        y = self.tuples()
-        x = other
-        for i in range(len(y[1])):
-            for j in range(len(y[1])):
-                add.append(y[i][j] * (1 / x))
-            add2.append(tuple(add))
-            add = []
-        return 'Matrix({})'.format(tuple(add2))
+        div = []
+        div2 = []
+        tupleOfSelf = self.tuples()
+        for i in range(len(tupleOfSelf[1])):
+            for j in range(len(tupleOfSelf[1])):
+                div.append(tupleOfSelf[i][j] * (1 / other))
+            div2.append(tuple(div))
+            div = []
+        return 'Matrix({})'.format(tuple(div2))
 
     def __mul__(self, other):
-        add2 = []
-        add3 = []
-        add4 = []
+        mul = []
+        mul2 = []
+        mul3 = []
         sum = 0
         if (type(self)) == int:
                 x = other.tuples()
@@ -81,62 +80,69 @@ class Matrix:
                     for j in range(len(x[1])):
                         for k in range(self):
                             sum += x[i][j]
-                        add3.append(sum)
+                        mul2.append(sum)
                         sum = 0
-                    add2.append(tuple(add3))
-                    add3 = []
+                    mul.append(tuple(mul2))
+                    mul2 = []
         if (type(other)) == int:
                 y = self.tuples()
                 for i in range(len(y[1])):
                     for j in range(len(y[1])):
                         for k in range(other):
                             sum += y[i][j]
-                        add3.append(sum)
+                        mul2.append(sum)
                         sum = 0
-                    add2.append(tuple(add3))
-                    add3 = []
+                    mul.append(tuple(mul2))
+                    mul2 = []
         else:
             y = self.tuples()
             x = other.tuples()
             for r in range(len(y[1])):
-                add4.append(0)
+                mul3.append(0)
             for t in range(len(y[1])):
-                add3.append(add4)
+                mul2.append(mul3)
             for i in range(len(y[1])):
                 for j in range(len(y[1])):
                     for k in range(len(y[1])):
-                        add3[i][j] += (y[i][k] * x[k][j])
-                add2.append(tuple(add3[i]))
-                add3 = []
-                add4 = []
+                        mul2[i][j] += (y[i][k] * x[k][j])
+                mul.append(tuple(mul2[i]))
+                mul2 = []
+                mul3 = []
                 for r in range(len(y[1])):
-                    add4.append(0)
+                    mul3.append(0)
                 for s in range(len(y[1])):
-                    add3.append(add4)
-        return 'Matrix({})'.format(tuple(add2))
+                    mul2.append(mul3)
+        return 'Matrix({})'.format(tuple(mul))
 
     def __eq__(self, other):
-        y = self.tuples()
-        x = other.tuples()
-        if x == y:
-            return True
-        return False
+        tupleOfSelf = self.tuples()
+        tupleOfOther = other.tuples()
+        return tupleOfOther == tupleOfSelf
 
     def __ne__(self, other):
-        y = self.tuples()
-        x = other.tuples()
-        if x == y:
-            return False
-        return True
+        tupleOfSelf = self.tuples()
+        tupleOfOther = other.tuples()
+        return not tupleOfOther == tupleOfSelf
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        try:
+            if self.__currentMatrix[self.count] in self.__currentMatrix:
+                self.count += 1
+                return self.__currentMatrix[self.count - 1]
+        except:
+            raise StopIteration
+
+    def __hash__(self):
+        return hash(str(self))
 
 
-a = Matrix(((1, 2, 5), (3, 5, 4), (7, 9, 3)))
-b = Matrix(((4, 2, 2), (9, 5, 1), (1, 2, 9)))
-print(b * 5)
-print(b * a)
-print(a / 3)
-for line in a:
-    print(line)
+
+
+
 
 
 
